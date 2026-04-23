3ccf639be544137091c6af6b8366cd8391c5407f
David Álvarez <pizzavibes11@gmail.com>
2026-04-23 14:28:26 -0500

    Eliminar carpeta Landing

--- tecnicentro-landing/src/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Footer = () => {
-  return (
-    <div id="footer">
-        <p>&copy; 2026 Tecnicentro. All rights reserved.</p>
-    </div>
-    )
-    }
-export default Footer;
